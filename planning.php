bbf51975fcce0d898c06921ab944daf897838fa6
RonaldDonfack <donfackronald13@gmail.com>
2023-12-11 10:26:14 +0100

    algorithme found trying to fix my xampp problem

--- planning.php
+++ planning.php
@@ -26,43 +26,39 @@ include 'conndb.php'
     </thead>
     <tbody>
         <?php
+        // Database connection details
+            $dsn = 'mysql:host=localhost;dbname=planmed';
+            $username = 'root';
+            $password = '';
+
+          // Create a new PDO instance
+             $conn = new PDO($dsn, $username, $password);
+
             $data = $conn->prepare('SELECT * from planning');
 
             $data->execute();
             $use = $data->fetchAll();
 
-            foreach($use as $med){
-               
-            }
+          
+
+               echo json_encode($use);
+           
+
+            // // <!-- changing the data received from the backend to a json file file to ease the manipulation in the javasctipt  -->
         
-        ?>
+            // $data = array(
+            //     'name' => 'John',
+            //     'age' => 30,
+            //     'city' => 'New York'
+            // );
 
-<!-- changing the data received from the backend to a json file file to ease the manipulation in the javasctipt  -->
-        <?php
-            $data = array(
-                'name' => 'John',
-                'age' => 30,
-                'city' => 'New York'
-            );
-
-            $json = json_encode($data);
-            echo "<script>var data = $json;</script>";
-            var_dump($json);
+            // $json = json_encode($data);
+            // echo "<script>var data = $json;</script>";
+            // var_dump($json);
         ?>
         <!-- javascript to manipulate the data by putting themm in corresponding colunmes  -->
         <script>
-            // Your JavaScript code goes here
-            var table = document.queryselector("table");
-            
-            const jourDeLeSemaine = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
-
-            for (var i = 0; i < table.rows.length; i++) {
-
-            if (table.rows[].cells[0].innerHTML === 'Jane') {
-                table.rows[i].cells[1].innerHTML = '32'; // Update Jane's age to 32
-                break;
-            }
-        }
+        
         </script>
     </tbody>
 </table>
